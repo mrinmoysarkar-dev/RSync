f6954815f4be066512c5d9336271d0143adef350
Mrinmoy Sarkar <mrinmoysarkar.personal@gmail.com>
2026-07-21 23:59:43 +0530

    Class Name Change

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const RSync());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class RSync extends StatelessWidget {
+  const RSync({super.key});
 
   @override
   Widget build(BuildContext context) {
